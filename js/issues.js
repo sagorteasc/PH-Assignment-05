7b9b30ecad449b2ec6683766094c4b968e7f679d
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-03-07 00:12:19 +0600

    add dynamic cards for all sections with proper details display

---
+++ js/issues.js
@@ -0,0 +1,53 @@
+// fetch issues
+const loadIssues = async () => {
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
+    const data = await res.json();
+    displayIssues(data.data);
+}
+
+// display issues
+const displayIssues = (issues) => {
+    const issuesContainer = document.getElementById('issues');
+
+    issues.forEach(issue => {
+        // console.log(issue);
+        const issueCards = document.createElement('div');
+        issueCards.innerHTML = `
+            <div class="card bg-base-100 h-full shadow-sm">
+                <div class="flex flex-col gap-0.5 p-6 flex-grow">
+                    <div class="flex justify-between mb-3">
+                        <img src=${issue.priority === 'high' ? "assets/Open-Status.png" : "assets/Closed-Status.png"} alt="">
+                        <h3 class="btn font-medium text-sm rounded-[100px] ${issue.priority === 'high' ? 'bg-[#FEECEC] text-[#EF4444]' : issue.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' : 'bg-[#EEEFF2] text-[#9CA3AF]'}">
+                        ${issue.priority === 'high' ? 'HIGH' : issue.priority === 'medium' ? 'MEDIUM' : 'LOW'}
+                        </h3>
+                    </div>
+                    <div class="space-y-1 mb-3 flex-grow">
+                        <h3 class="font-semibold text-sm text-[#1F2937]">${issue.title}</h3>
+                        <p class="text-[12px] text-[#64748B]">${issue.description}</p>
+                    </div>
+                    <div class="flex gap-1 items-center whitespace-nowrap mb-3">
+                        ${labels(issue.labels)}
+                    </div>
+                    <hr class="text-gray-300 mb-3">
+                    <div class="text-[#64748B] text-[12px] flex justify-between items-center">
+                        <h3 class="">#${issue.id} ${issue.author}</h3>
+                        <p class="">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                    </div>
+                    <div class="text-[#64748B] text-[12px] flex justify-between items-center">
+                        <h3 class="">Assignee: ${issue.assignee === '' ? 'No Data Found' : issue.assignee}</h3>
+                        <p class="">${new Date(issue.updatedAt).toLocaleDateString()}</p>
+                    </div>
+                </div>
+            </div>
+        `
+        issuesContainer.appendChild(issueCards);
+    });
+}
+
+// labels array
+const labels = (items) => {
+    const label = items.map(item => `<span class="px-1.5 py-2 bg-[#FFF8DB] text-[#D97706] rounded-[100px] font-medium text-[12px]">${item.toUpperCase()}</span>`)
+    return (label.join(" "));
+}
+
+loadIssues();
