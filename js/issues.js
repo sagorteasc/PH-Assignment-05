a7d36e985b915aba1705975696ae10e0f0666879
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-03-07 21:56:15 +0600

    add dynamic modal display when a card is clicked

--- js/issues.js
+++ js/issues.js
@@ -13,29 +13,29 @@ const displayIssues = (issues) => {
         // console.log(issue);
         const issueCards = document.createElement('div');
         issueCards.innerHTML = `
-            <div class="card bg-base-100 h-full shadow-sm border-t-5 ${issue.status === 'open' ? 'border-t-[rgb(99,230,190)]' : 'border-t-[rgb(177,151,252)]'}">
+            <div onclick="detailsModal(${issue.id})" class="card bg-base-100 h-full shadow-sm border-t-5 ${issue.status === 'open' ? 'border-t-[rgb(99,230,190)]' : 'border-t-[rgb(177,151,252)]'}">
                 <div class="flex flex-col gap-0.5 p-6 flex-grow">
                     <div class="flex justify-between mb-3">
                         <img src=${issue.status === 'open' ? "assets/Open-Status.png" : "assets/Closed-Status.png"} alt="">
-                        <h3 class="btn font-medium text-sm rounded-[100px] ${issue.priority === 'high' ? 'bg-[#FEECEC] text-[#EF4444]' : issue.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' : 'bg-[#EEEFF2] text-[#9CA3AF]'}">
+                        <h3 class="px-4 py-2 font-medium text-sm rounded-[100px] ${issue.priority === 'high' ? 'bg-[#FEECEC] text-[#EF4444]' : issue.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' : 'bg-[#EEEFF2] text-[#9CA3AF]'}">
                         ${issue.priority === 'high' ? 'HIGH' : issue.priority === 'medium' ? 'MEDIUM' : 'LOW'}
                         </h3>
                     </div>
                     <div class="space-y-1 mb-3 flex-grow">
                         <h3 class="font-semibold text-sm text-[#1F2937]">${issue.title}</h3>
-                        <p class="text-[12px] text-[#64748B]">${issue.description}</p>
+                        <p class="text-xs text-[#64748B]">${issue.description}</p>
                     </div>
                     <div class="flex gap-1 items-center whitespace-nowrap mb-3">
                         ${labels(issue.labels)}
                     </div>
                     <hr class="text-gray-300 mb-3">
-                    <div class="text-[#64748B] text-[12px] flex justify-between items-center">
-                        <h3 class="">#${issue.id} ${issue.author}</h3>
-                        <p class="">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                    <div class="text-[#64748B] text-xs flex justify-between items-center">
+                        <h3>#${issue.id} ${issue.author}</h3>
+                        <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
                     </div>
-                    <div class="text-[#64748B] text-[12px] flex justify-between items-center">
-                        <h3 class="">Assignee: ${issue.assignee === '' ? 'No Data Found' : issue.assignee}</h3>
-                        <p class="">${new Date(issue.updatedAt).toLocaleDateString()}</p>
+                    <div class="text-[#64748B] text-xs flex justify-between items-center">
+                        <h3>Assignee: ${issue.assignee === '' ? 'Unknown' : issue.assignee}</h3>
+                        <p>${new Date(issue.updatedAt).toLocaleDateString()}</p>
                     </div>
                 </div>
             </div>
@@ -46,7 +46,7 @@ const displayIssues = (issues) => {
 
 // labels array
 const labels = (items) => {
-    const label = items.map(item => `<span class="px-1.5 py-2 bg-[#FFF8DB] text-[#D97706] rounded-[100px] font-medium text-[12px]">${item.toUpperCase()}</span>`)
+    const label = items.map(item => `<span class="px-1.5 py-2 bg-[#FFF8DB] text-[#D97706] rounded-[100px] font-medium text-xs">${item.toUpperCase()}</span>`)
     return (label.join(" "));
 }
 
---
+++ js/modal.js
@@ -0,0 +1,41 @@
+const detailsModal = async (id) => {
+    try {
+        const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+        const data = await res.json()
+        const singleDetail = data.data;
+
+        const modalBox = document.getElementById('issue-detail');
+        const modalContainer = document.getElementById('modal-container');
+        modalContainer.innerHTML = `
+            <div class="space-y-3">
+                <h3 class="font-bold text-2xl text-[#1F2937]">${singleDetail.title}</h3>
+                <div class="flex items-center gap-2">
+                    <button class="px-4 py-2 text-white rounded-full ${singleDetail.status === 'open' ? 'bg-[#00A96E]' : 'bg-[#A855F7]'}">${singleDetail.status === 'open' ? 'Opened' : 'Closed'}</button>
+                    <span class="text-[#64748B]"> • </span>
+                    <p class="text-xs text-[#64748B]">Opened by ${singleDetail.author}</p>
+                    <span class="text-[#64748B]"> • </span>
+                    <p class="text-xs text-[#64748B]">${new Date(singleDetail.createdAt).toLocaleDateString()}</p>
+                </div>
+                <div class="flex gap-1 items-center whitespace-nowrap">
+                    ${labels(singleDetail.labels)}
+                </div>
+                <p class="text-[#64748B]">${singleDetail.description}</p>
+                <div class="bg-[#F8FAFC] rounded-lg p-4 flex justify-between shadow-sm">
+                    <div>
+                        <p class="text-[#64748B]">Assignee:</p>
+                        <h3 class="text-[#1F2937] font-semibold">${singleDetail.assignee === '' ? 'Unknown' : singleDetail.assignee}</h3>
+                    </div>
+                    <div class="flex flex-col justify-center items-center">
+                        <p class="text-[#64748B]">Priority:</p>
+                        <h3 class="rounded-full w-fit px-4 py-2 text-white ${singleDetail.priority === 'high' ? 'bg-[#EF4444]' : singleDetail.priority === 'medium' ? 'bg-[#F59E0B]' : 'bg-[#9CA3AF]'}">${singleDetail.priority === 'high' ? 'HIGH' : singleDetail.priority === 'medium' ? 'MEDIUM' : 'LOW'}</h3>
+                    </div>
+                </div>
+            </div>
+        `
+        modalBox.showModal();
+    }
+
+    catch (err) {
+        alert('Error', err);
+    }
+}
